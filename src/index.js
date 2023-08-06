9272697afafc41e2a2ba99066f1d0c114c2135af
Yurick <YJakse789@gmail.com>
2023-08-06 16:32:52 -0400

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-const init = () => {
-  
-}
+const inputForm = document.querySelector("form")
+inputForm.addEventListener("submit", (e)=>{
+  e.preventDefault()
+  const input = document.querySelector("input#searchByID")
 
-document.addEventListener('DOMContentLoaded', init);
+  fetch(`http://127.0.0.1:3000/movies/${input.value}`)
+    .then((response)=>response.json())
+    .then((data)=>{
+      const title = document.querySelector("section#movieDetails h4")
+      const summary = document.querySelector("section#movieDetails p")
+      title.textContent = data.title
+      summary.textContent = data.summary
+    })
+})
